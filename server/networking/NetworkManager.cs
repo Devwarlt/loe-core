523fc784f0774e5408c29c985fa0d1b1c79283df
Devwarlt <devwarlt@loesoft.org>
2018-08-14 23:29:44 -0300

    WIP: server network manager.

---
+++ server/networking/NetworkManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Sockets;
+
+namespace LoESoft.Server.networking
+{
+    public class NetworkManager
+    {
+        private Socket _socket { get; set; }
+
+        public NetworkManager()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+    }
+}
