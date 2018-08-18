d1bb6a1fd833a1cd93e4774989e45db8f22a33c5
Demon <34493949+Demon09090123@users.noreply.github.com>
2018-08-18 06:40:40 -0400

    Merge Commit

---
+++ client/Assets/AssetReader.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoESoft.Client.Assets
+{
+    public static class AssetReader
+    {
+        public static Dictionary<string, ImageSet> _images;
+
+        static AssetReader()
+        {
+            _images = new Dictionary<string, ImageSet>();
+        }
+
+        public static void Load()
+        {
+            Console.WriteLine("Loading Assets...");
+
+
+            Console.WriteLine("Finished Loading Assets...");
+        }
+
+
+    }
+
+    public class ImageSet
+    {
+        private List<Texture2D> Images;
+
+
+        public ImageSet()
+        {
+            Images = new List<Texture2D>();
+        }
+
+        public void AddImage(Texture2D image) => Images.Add(image);
+    }
+}
