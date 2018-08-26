ec9ff13e399f35a0fec9e3716c582153f5f2fe2a
Slendergo <survivalistrivalist@gmail.com>
2018-08-26 16:34:02 +0100

    Started Launcher
    Some test designs of the launcher

---
+++ LoeSoft.Launcher/LauncherForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace LoESoft.Launcher
+{
+    partial class LauncherForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                components?.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.ButtonsPanel = new System.Windows.Forms.Panel();
+            this.TestCustomButton = new LoeSoft.Launcher.Controls.CustomButton();
+            this.ButtonsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ButtonsPanel
+            // 
+            this.ButtonsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ButtonsPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ButtonsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ButtonsPanel.Controls.Add(this.TestCustomButton);
+            this.ButtonsPanel.Location = new System.Drawing.Point(0, 0);
+            this.ButtonsPanel.Name = "ButtonsPanel";
+            this.ButtonsPanel.Size = new System.Drawing.Size(175, 600);
+            this.ButtonsPanel.TabIndex = 0;
+            // 
+            // TestCustomButton
+            // 
+            this.TestCustomButton.Location = new System.Drawing.Point(0, 0);
+            this.TestCustomButton.Name = "TestCustomButton";
+            this.TestCustomButton.Size = new System.Drawing.Size(175, 50);
+            this.TestCustomButton.TabIndex = 1;
+            this.TestCustomButton.Text = "Test";
+            // 
+            // LauncherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(1200, 600);
+            this.Controls.Add(this.ButtonsPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "LauncherForm";
+            this.Text = "LoE Launcher";
+            this.ButtonsPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ButtonsPanel;
+        private LoeSoft.Launcher.Controls.CustomButton TestCustomButton;
+    }
+}
+
