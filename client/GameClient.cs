7ddd3ee3fad943c53cbafd261cc39412253bb6d4
Devwarlt <devwarlt@loesoft.org>
2018-09-18 00:23:32 -0300

    :warning: Make sure this line is active!

--- client/GameClient.cs
+++ client/GameClient.cs
@@ -104,7 +104,7 @@ namespace LoESoft.Client
 
 #if DEBUG
             // Rollbar error analytics for developers only.
-            //RollbarLocator.RollbarInstance.Error(e);
+            RollbarLocator.RollbarInstance.Error(e);
 #endif
         }
     }
