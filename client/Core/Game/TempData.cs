7d07b813acfc53b6ccae07d25d1c94a26de40ada
Devwarlt <devwarlt@loesoft.org>
2018-09-28 02:46:09 -0300

    Another fix #88

---
+++ client/Core/Game/TempData.cs
@@ -0,0 +1,12 @@
+﻿namespace LoESoft.Client.Core.Game
+{
+    public class TempData
+    {
+        public string[,] Tiles { get; set; }
+
+        public TempData()
+        {
+            Tiles = new string[16, 16]; //temporary
+        }
+    }
+}
