d6570030e3c4107c5a445219f89a39527c05c592
Devwarlt <devwarlt@loesoft.org>
2018-11-26 00:19:48 -0200

    Starting development of Xml Manager software

---
+++ xmlmanager/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LoESoft - XmlManager")]
+[assembly: AssemblyProduct("MapEditor")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("A BRME game assets application.")]
+[assembly: AssemblyCompany("LoESoft Games")]
+[assembly: AssemblyCopyright("Copyright © LoESoft Games 2018")]
+[assembly: AssemblyTrademark("BRME")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3155b7b4-98ce-435b-9f19-ed476be03757")]
+[assembly: AssemblyVersion("0.1.5")]
+[assembly: AssemblyFileVersion("0.1.5")]
+[assembly: NeutralResourcesLanguage("en-US")]
